2132185980058812e89c4054e6c75a7c9dc4d03b
Dünya Kılavuz <dunyakilavuz@gmail.com>
2022-09-07 15:29:07 +0300

    trying to get it right

--- puttodocs.sh
+++ puttodocs.sh
@@ -1,2 +1,28 @@
+flutter build web
+
+if [ $? -eq 0 ]; then
+    echo "Flutter build web complete!"
+else
+    echo "Flutter build web failed, exiting..."
+    exit 1
+fi
+
 rm -r docs/* #Remove everything in docs
-cp -a web/ docs/ #Copy everything in web --> docs
+
+if [ $? -eq 0 ]; then
+    echo "Emptied the /docs folder."
+else
+    echo "Failed to empty the /docs folder, exiting..."
+    exit 1
+fi
+
+cp -a build/web/ docs/ #Copy everything in build/web --> docs
+
+if [ $? -eq 0 ]; then
+    echo "Copied build/web --> docs."
+else
+    echo "Failed to copy build/web --> docs, exiting..."
+    exit 1
+fi
+echo "Finished."
+exit 0
